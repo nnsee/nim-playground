a48202f9c01c4b2a86b837a5dbddb05aa33c6150
PMunch <peterme@peterme.net>
2019-10-15 11:26:16 +0200

    Fix issue with old version of git

--- docker/updateimages.sh
+++ docker/updateimages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 missing=$(uniq -u <(sort <(cat ignored <(git ls-remote --tags git://github.com/nim-lang/nim.git | sed -n 's/.*refs\/tags\/\(.*\)^{}/\1/p') <(docker images | sed -n 's/virtual_machine *\(v[^ ]*\).*/\1/p'))))
-latest=$(git ls-remote --sort=v:refname --tags git://github.com/nim-lang/nim.git | sed -n 's/.*refs\/tags\/\(.*\)^{}/\1/p' | tail -n1)
+latest=$(git ls-remote --tags git://github.com/nim-lang/nim.git | sed -n 's/.*refs\/tags\/\(.*\)^{}/\1/p' | sed 's/v/0./' | sort -t. -n -k1,1 -k2,2 -k3,3 -k4,4 | sed 's/0./v/' | tail -n1)
 
 while read -r line; do
   if [ ! -z "$line" ]; then
